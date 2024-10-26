35adba63e2573d55bdef6012b75ad050d7f05d00
Dat Tran <dattran@zaitenllc.com>
2024-10-26 16:31:19 +0700

    add lcov reporter sonar report

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: ["./vitest.setup.ts"],
     coverage: {
-      reporter: ["text", "json", "html"],
+      reporter: ["text", "json", "html", "lcov"],
       enabled: true,
       exclude: [
         ...configDefaults.exclude,

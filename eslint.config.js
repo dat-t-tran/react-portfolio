bf3be193547e693e4b00a79f9a8caa88dfe58b0b
Dat Tran <dattran@zaitenllc.com>
2024-10-26 17:16:17 +0700

    update README.md and add sonar config

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import jsxA11y from "eslint-plugin-jsx-a11y";
+import { vitest } from "vitest";
 
 export default tseslint.config(
   {
@@ -32,9 +33,11 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      vitest,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...vitest.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
